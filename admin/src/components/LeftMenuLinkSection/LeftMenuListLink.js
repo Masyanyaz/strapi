8b0e5d4df019441c5d21e22d40c4e7819ffd6df3
Maksim <tigramasya96@yandex.ru>
2020-07-26 14:06:12 +0300

    update strapi

--- admin/src/components/LeftMenuLinkSection/LeftMenuListLink.js
+++ admin/src/components/LeftMenuLinkSection/LeftMenuListLink.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const LeftMenuListLink = styled.div`
-  max-height: 300px;
+  max-height: 450px;
   margin-bottom: 19px;
   margin-right: 28px;
   overflow: auto;
